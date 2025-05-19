ab361800fd4a04569da22bd3aa1a902f02f7a617
SohaibKhan007 <sohaibkhanatwork@gmail.com>
2025-05-19 10:51:09 +0530

    project done

--- back-end(Spring-boot)/src/main/java/com/url/shortener/controller/AuthController.java
+++ back-end(Spring-boot)/src/main/java/com/url/shortener/controller/AuthController.java
@@ -19,12 +19,12 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/public/login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(userService.authenticateUser(loginRequest));
     }
 
     @PostMapping("/public/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setPassword(registerRequest.getPassword());
